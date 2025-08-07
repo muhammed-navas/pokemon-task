691b4624a2b528b15e557c47ac3d4381d9d64918
muhammed navas <muhammednavas6287@gmail.com>
2025-08-07 15:47:43 +0530

    update queryClient file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
-import './App.css'
-import Header from './components/layout/Header'
-import DiscoveryPage from './page/DiscoveryPage'
+import { QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+import Header from './components/layout/Header';
+import DiscoveryPage from './page/DiscoveryPage';
+import { queryClient } from './lib/react-query';
 
 function App() {
   const [currentPage, setCurrentPage] = useState<'discovery' | 'collection'>('discovery');
   return (
-    <div className="min-h-screen bg-gray-50">
-    <Header
-      currentPage={currentPage}
-      onPageChange={setCurrentPage}
-    />
-    <main className="pt-16">
-        <DiscoveryPage />
-    </main>
-  </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen bg-gray-50">
+        <Header
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
+        <main className="pt-16">
+          <DiscoveryPage />
+        </main>
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/react-query.ts'
+import { queryClient } from './queryClient/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
